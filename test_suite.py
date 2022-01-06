e941c0bdc137c9971d6c94e0ec81a5a25f26dea0
O . O <ojustino@users.noreply.github.com>
2022-01-05 22:04:07 -0500

    Revamped test_suite.py; remade reference images
    The previous tests failed over time because Tiingo's adjusted price
    columns change after dividends and splits. I wrote a function to
    stabilize them and integrated it into the existing tests. The results
    and reference images changed as a result.

--- test_suite.py
+++ test_suite.py
@@ -1,3 +1,4 @@
+import numpy as np
 import os
 import pandas as pd
 import pytest
@@ -12,32 +13,86 @@ from strategies import BuyAndHoldStrategy, SMAStrategy, VolTargetStrategy
 # their expected values. Individual functions may also test other things; these
 # are mentioned in their docstrings.
 
-# NOTE: When writing tests, ensure there are no splits ('splitFactor' column
-# always == 1) in any assets over the chosen simulation periods!
-
-def _use_unadjusted(self):
+def _rewind_prices(self):
     '''
-    Use 'open' and 'close' columns in price dataFrames instead of 'adjClose' and
-    'adjOpen'. The latter two are backwards-adjusted over time for dividends,
-    splits, and so on, which makes them inappropriate for tests where we seek
-    consistent results over time. **Keep in mind that this means these tests
-    don't count as accurate backtests, just as examinations of each Strategy's
-    inner workings.**
+    Takes a dataFrame from a symbol's 'df' key in the `assets` dictionary of a
+    Strategy instance (e.g. bnh.assets['AAPL']['df']) and adjusts the
+    dataFrame's 'adj' columns (close, high, low, open) to approximate how they
+    would have looked on the Strategy instance's end date instead of on
+    whichever date the instance was created.
+
+    This is useful because, as seen in cron job results, Tiingo's own 'adj'
+    column values vary over time as dividends and stock splits occur. To get
+    consistent results for testing purposes, you must either use the unadjusted
+    columns (ignoring dividends and splits) or figure out how to reproducibly
+    adjust the price values to your own liking (this!).
 
     Assigned to Strategy instances as a new method through MethodType.
+
+    NOTE: Past dates' 'adj' values won't exactly match the originals for
+    Strategy instances with end dates on your present date. This is likely
+    because the original 'close' column is rounded to only the hundredths
+    place. It should only be a small discrepancy: for AAPL from 1993-12-27 to
+    2021-02-10, the difference in 'adjClose' values is 3e-5 on the first day.
+
+    NOTE: VALUES WILL NEED TO BE CHANGED AGAIN ONCE run_time_loop IS CORRECTED
+    SUCH THAT IT DOESN'T DOUBLE COUNT DIVIDENDS.
     '''
     for _, val in self.assets.items():
-        val['df'].rename(columns={'adjOpen': 'OG_adjO', 'adjClose': 'OG_adjC',
-                                  'open': 'adjOpen', 'close': 'adjClose'},
-                         inplace=True)
+        df = val['df'].copy()
+        cols = ['close', 'high', 'low', 'open']
+
+        # split factor for each date through cumulative product
+        # (inspiration from https://stackoverflow.com/questions/62130566/)
+        tot_splits = df['splitFactor'].cumprod().values
+        tot_splits_cast = np.tile(tot_splits, (len(cols), 1)).T
+
+        # get dates and amounts of dividends
+        div_dts = df[df['divCash'] != 0].index
+        div_amts = df.loc[div_dts, 'divCash'].values
+        div_amts_cast = np.tile(div_amts, (len(cols), 1)).T
+
+        # get indices of days that precede dividend ex-dates
+        pre_div_inds = df.reset_index()[df.index.isin(div_dts)].index - 1
+
+        # get dates and prices of these pre-dividend indices
+        pre_div_dts = df.iloc[pre_div_inds].index
+        pre_div_prices = df.iloc[pre_div_inds][cols]
+        # (pre_div_closes, pre_div_highs,
+        #  pre_div_lows, pre_div_opens) = df.iloc[pre_div_inds][cols].T.values
+
+        # calculate dividend adjustments on close prices, then get
+        # cumulative product of these adjustments going backward in time
+        tot_div_adjs = ( (pre_div_prices - div_amts_cast)
+                         / pre_div_prices)[::-1].cumprod()[::-1]
+
+        # apply the dividend adjustments to previous prices
+        divFactors = pd.DataFrame(index=df.index, columns=cols, data=1)
+
+        for i, dt in enumerate(pre_div_dts):
+            if i == 0:
+                divFactors.loc[:pre_div_dts[i]] = tot_div_adjs.iloc[i].T.values
+            else:
+                divFactors.loc[div_dts[i-1] : pre_div_dts[i],
+                               :] = tot_div_adjs.iloc[i].T.values
+
+        # rename Tiingo's adj columns
+        adj_cols = ['adj' + c.title() for c in cols]
+        old_adj_cols = ['OG_' + c.title() for c in adj_cols]
+        renames = {adj: old for adj, old in zip(adj_cols, old_adj_cols)}
+
+        val['df'].rename(columns=renames, inplace=True)
+
+        # add the readjusted price colums to the original dataFrame
+        val['df'][adj_cols] = tot_splits_cast * divFactors * df[cols]
 
 def _compare_figs(ax, img_root):
     '''
     Compare axes from plot_results() or plot_assets() in the test function's
     Strategy to those from a corresponding reference images.
     '''
-    ref_img = f"{img_root}_ref.png"
-    test_img = f"{img_root}_test.png"
+    ref_img = f"tests/{img_root}_ref.png"
+    test_img = f"tests/{img_root}_test.png"
 
     ax.figure.savefig(test_img, facecolor='w', dpi=75)
     msg = compare_images(ref_img, test_img, tol=15)
@@ -59,7 +114,7 @@ def test_bnh():
     pf1 = PortfolioMaker(sat_frac=0)
     pf1.add_ticker('MCD', 1/3)
     pf1.add_ticker('TGT', 1/3)
-    pf1.add_ticker('PEP', 1/3)
+    pf1.add_ticker('CPB', 1/3)
     pf1.add_ticker('SPY', 1, label='benchmark')
 
     # initialize Strategy and swap price columns
@@ -69,29 +124,31 @@ def test_bnh():
                              cash=10000, reinvest_dividends=False,
                              tot_rb_freq=12, target_rb_day=0)
 
-    bnh._use_unadjusted = MethodType(_use_unadjusted, bnh)
-    bnh._use_unadjusted()
+    bnh._rewind_prices = MethodType(_rewind_prices, bnh)
+    bnh._rewind_prices()
 
     # append another rebalance date to test the process
-    nu_prices = {'MCD': 117.25, 'TGT': 71.84, 'PEP': 98.56, 'SPY': 200.49}
+    nu_prices = {'MCD': 117.25, 'TGT': 71.84, 'CPB': 43.83, 'SPY': 200.49}
     bnh.append_date(pd.Timestamp(2016, 1, 4), nu_prices)
 
     # run simulation
     bnh.begin_time_loop()
 
     # compare portfolio values to expectations
-    exp_pf_val = 43195.082500000004
-    exp_bnch_val = 18642.541540100003
+    exp_pf_val = 62600.62671775877
+    exp_bnch_val = 26162.867776345214
     test_pf_val = bnh.portfolio_value()
     test_bnch_val = bnh.portfolio_value(main_portfolio=False)
 
-    assert test_pf_val == exp_pf_val, 'main portfolio value mismatch!'
-    assert test_bnch_val == exp_bnch_val, 'benchmark portfolio value mismatch!'
+    np.testing.assert_almost_equal(test_pf_val, exp_pf_val, decimal=4,
+                                   err_msg=': main portfolio')
+    np.testing.assert_almost_equal(test_bnch_val, exp_bnch_val, decimal=4,
+                                   err_msg=': benchmark portfolio')
 
     # compare portfolios' share counts to expectations
-    exp_shares = {'MCD': 122.0, 'TGT': 200.0, 'PEP': 146.0, 'SPY': 92.0}
+    exp_shares = {'MCD': 177.0, 'TGT': 290.0, 'CPB': 476.0, 'SPY': 130.0}
     tst_shares = {key : val['shares'] for key,val in bnh.assets.items()}
-    assert tst_shares == exp_shares, 'portfolio share count mismatch!'
+    assert tst_shares == exp_shares, 'portfolio share count mismatch'
 
     # compare semi-log version of results plot to reference
     img_root = 'bnh'
@@ -100,7 +157,7 @@ def test_bnh():
 
     # compare semi-log version of ticker plot to reference
     img_root = 'bnh_tk'
-    ax = bnh.plot_assets('MCD', 'TGT', 'PEP', 'SPY',
+    ax = bnh.plot_assets('MCD', 'TGT', 'CPB', 'SPY',
                          logy=True, return_plot=True, verbose=False)
     _compare_figs(ax, img_root)
 
@@ -132,29 +189,35 @@ def test_sma():
                       cash=1738.29, reinvest_dividends=True,
                       sat_rb_freq=365.25, tot_rb_freq=12, target_rb_day=8)
 
-    sma._use_unadjusted = MethodType(_use_unadjusted, sma)
-    sma._use_unadjusted()
+    sma._rewind_prices = MethodType(_rewind_prices, sma)
+    sma._rewind_prices()
 
     # run simulation
     sma.begin_time_loop()
 
     # compare portfolio values to expectations
-    exp_pf_val = 15427.220752005629
-    exp_bnch_val = 10753.115461066573
+    exp_pf_val = 14038.487133948604
+    exp_bnch_val = 10898.582343484206
     test_pf_val = sma.portfolio_value()
     test_bnch_val = sma.portfolio_value(main_portfolio=False)
 
-    assert test_pf_val == exp_pf_val, 'main portfolio value mismatch!'
-    assert test_bnch_val == exp_bnch_val, 'benchmark portfolio value mismatch!'
+    np.testing.assert_almost_equal(test_pf_val, exp_pf_val, decimal=4,
+                                   err_msg=': main portfolio')
+    np.testing.assert_almost_equal(test_bnch_val, exp_bnch_val, decimal=4,
+                                   err_msg=': benchmark portfolio')
 
     # compare portfolios' share counts to expectations
-    exp_shares = {'SCHG': 38.84719129938834, 'SCHM': 20.764844216561272,
-                  'EFG': 12.10906921207649, 'ACES': 12.177886217386732,
-                  'BIV': 14.625274268414007, 'LQD': 3.042060286037833,
-                  'FB': 0.0, 'TQQQ': 48.0, 'TLT': 0.0,
-                  'SPY': 18.2881180227943, 'AGG': 35.52453216798228}
+    exp_shares = {'SCHG': 35.83601442282118, 'SCHM': 18.748962642069873,
+                  'EFG': 11.109352277298585, 'ACES': 11.169197042734888,
+                  'BIV': 12.618948407993443, 'LQD': 3.040265631432126,
+                  'FB': 0.0, 'TQQQ': 43.0, 'TLT': 0.0,
+                  'SPY': 19.300408108960365, 'AGG': 36.53405970562885}
     tst_shares = {key : val['shares'] for key,val in sma.assets.items()}
-    assert tst_shares == exp_shares, 'portfolio share count mismatch!'
+
+    for key in tst_shares.keys():
+        np.testing.assert_almost_equal(tst_shares[key], exp_shares[key],
+                                       decimal=4,
+                                       err_msg=f": {key} share count")
 
     # compare results plot to reference
     img_root = 'sma'
@@ -187,25 +250,27 @@ def test_vlt():
                             cash=5500, reinvest_dividends=False,
                             sat_rb_freq=12, tot_rb_freq=4, target_rb_day=-3)
 
-    vlt._use_unadjusted = MethodType(_use_unadjusted, vlt)
-    vlt._use_unadjusted()
+    vlt._rewind_prices = MethodType(_rewind_prices, vlt)
+    vlt._rewind_prices()
 
     # run simulation
     vlt.begin_time_loop()
 
     # compare portfolio values to expectations
-    exp_pf_val = 5942.462709999999
+    exp_pf_val = 6134.986341181822
     exp_bnch_val = 6312.630000000001
     test_pf_val = vlt.portfolio_value()
     test_bnch_val = vlt.portfolio_value(main_portfolio=False)
 
-    assert test_pf_val == exp_pf_val, 'main portfolio value mismatch!'
-    assert test_bnch_val == exp_bnch_val, 'benchmark portfolio value mismatch!'
+    np.testing.assert_almost_equal(test_pf_val, exp_pf_val, decimal=4,
+                                   err_msg=': main portfolio value')
+    np.testing.assert_almost_equal(test_bnch_val, exp_bnch_val, decimal=4,
+                                   err_msg=': benchmark portfolio value')
 
     # compare portfolios' share counts to expectations
-    exp_shares = {'WMT': 16.0, 'SSO': 30.0, 'TLT': 2.0, 'GLD': 47.0}
+    exp_shares = {'WMT': 16.0, 'SSO': 31.0, 'TLT': 2.0, 'GLD': 47.0}
     tst_shares = {key : val['shares'] for key,val in vlt.assets.items()}
-    assert tst_shares == exp_shares, 'portfolio share count mismatch!'
+    assert tst_shares == exp_shares, 'portfolio share count mismatch'
 
     # compare results plot to reference
     img_root = 'vlt'
